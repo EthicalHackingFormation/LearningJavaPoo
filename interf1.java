6749cefd4ff93c909977569a8569fac78e4aa42b
EthicalHackingFormation <44448220+EthicalHackingFormation@users.noreply.github.com>
2023-07-26 12:01:01 +0300

    Update interf1.java

--- interf1.java
+++ interf1.java
@@ -1,7 +1,31 @@
 package Poo;
 
+interface A{
+    //all variable in interface is final (constant) and static (no need to instanciate)
+    //final because the class dont have access to variable, only methods
+    int age= 44 ;
+    String area= "Mumbai";
+
+    void show(); //all methods in interface are by default Public and Abstract
+}
+
+//if you dont want to override the method show() of A, you can change B to Abstract, else you need to implements show() in B
+ class B implements A {
+    
+    public void show() {
+        System.out.println("Showing....");
+    }
+
+}
+
 public class interf1 {
      public static void main(String[] args) {
-        
+            A obj;
+            obj = new B();
+            obj.show();
+
+            //you can call static variable
+            System.out.println(A.area);
+            System.out.println(A.age);
      }
 }
